3009c819ff39c5a85afae897dedd2f40b3fa0b8c
David Miotti <david@muxumuxu.com>
2018-03-11 16:09:48 +0100

    Initial Commit

---
+++ Chess Engine/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Chess Engine
+//
+//  Created by David Miotti on 11/03/2018.
+//  Copyright © 2018 Muxu.Muxu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
